35f1c4d950203c4f4c7e71e73e94e329a1ea292c
Arlisson Costa <arlisson.fs13@gmail.com>
2023-02-18 14:32:10 -0300

    including divider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import Header from "@/components/Header";
-import Light from "@/components/Light";
 import Presentation from "@/components/Presentation";
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <section className="home w-screen h-screen">
         <Header />
         <Presentation />
+        <div className="divider w-full h-64"></div>
       </section>
       <section className="h-[3000px]"></section>
     </>
   );
-}
+};
+
+export default Home;
