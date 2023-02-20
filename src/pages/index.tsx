4c2e587475441eeba63767d351f17ab402e6ec6b
Arlisson Costa <arlisson.fs13@gmail.com>
2023-02-20 04:35:29 -0300

    changing favicon and improving SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,26 +5,42 @@ import ProfessionalExperience from "@/components/ProfessionalExperience";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import bg from "@/assets/images/bg-home.jpg";
+import Head from "next/head";
 
 const Home = () => {
   return (
-    <main>
-      <section
-        style={{ backgroundImage: `url(${bg.src})` }}
-        className="home w-screen h-screen"
-        id="home"
-      >
-        <Header />
-        <Presentation />
-        <div className="divider w-full h-64"></div>
-      </section>
-      <div className="mx-auto max-w-7xl px-2">
-        <Skills />
-        <Projects />
-        <ProfessionalExperience />
-        <Contact />
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Portfólio | Arlisson Costa - Desenvolvedor Front-end</title>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          name="description"
+          content="Desenvolvedor com foco em Front-end utilizando React e Next.js."
+        />
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript, Front-end, Back-end, Desenvolvedor Web, React, Next.js, Docker"
+        />
+        <meta name="author" content="Arlisson Costa" />
+      </Head>
+      <main>
+        <section
+          style={{ backgroundImage: `url(${bg.src})` }}
+          className="home w-screen h-screen"
+          id="home"
+        >
+          <Header />
+          <Presentation />
+          <div className="divider w-full h-64"></div>
+        </section>
+        <div className="mx-auto max-w-7xl px-2">
+          <Skills />
+          <Projects />
+          <ProfessionalExperience />
+          <Contact />
+        </div>
+      </main>
+    </>
   );
 };
 
