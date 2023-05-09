dc3eaba042b9dbbcdbf2492802bf62c1a2bc3635
Arlisson Costa <arlisson.fs13@gmail.com>
2023-05-09 20:28:15 -0300

    changing text about me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,18 +1,15 @@
 const AboutMe = () => {
   return (
-    <section className="mb-44">
+    <section className="mb-44 flex items-center flex-col">
       <h1 className="section-title">Sobre mim</h1>
-      <p className="text-white text-center text-sm">
-        Desenvolvedor com foco em Front-end utilizando React / Next.js. Possuo
-        também experiência em projetos gerenciados por metodologias ágeis, o que
-        me permitiu aprender a trabalhar de forma eficiente em equipe, priorizar
-        tarefas e entregar projetos de alta qualidade dentro do prazo. Estou
-        sempre aberto a novos conhecimentos e tecnologias, buscando me manter
-        atualizado com as tendências do mercado. Minha paixão por programação me
-        motiva a trabalhar arduamente e a aprimorar minhas habilidades
-        constantemente, permitindo-me desenvolver soluções inovadoras e
-        eficientes para meus clientes e usuários. Estou sempre em busca de novos
-        desafios e oportunidades para crescer profissionalmente.
+      <p className="text-white text-center max-w-xl">
+        Desenvolvedor front-end com foco em React.js / Next.js. Possuo também
+        experiência em projetos gerenciados por metodologias ágeis, o que me
+        permitiu aprender a trabalhar de forma eficiente em equipe, priorizar
+        tarefas e entregar projetos de alta qualidade dentro do prazo.
+        Atualmente estou me dedicando a estudar Node.js e desenvolvimento
+        mobile. Acredito que isso me tornará um desenvolvedor mais completo e
+        capaz de enfrentar os desafios da indústria de tecnologia.
       </p>
     </section>
   );
